95205eaa4197d78fabda3128edf53ba1efe83a0f
Javier Pérez López <javier.perezlopez6@icloud.com>
2025-10-11 18:36:06 +0200

    Add exercise 2

--- 01-arrow-functions/01-arrow-functions.js
+++ 01-arrow-functions/01-arrow-functions.js
@@ -7,3 +7,13 @@ exercise1Test.addEventListener('click', () => {
     const result = add(2, 3);
     console.log(result); // 5
 });
+
+// Exercise 2
+const randomNumber = () => Math.floor(Math.random() * 101);
+
+// HTML part to test
+const exercise2Test = document.getElementById('run-exercise-2-btn');
+exercise2Test.addEventListener('click', () => {
+    const number = randomNumber();
+    console.log(number); // Random number between 0 and 100
+});
