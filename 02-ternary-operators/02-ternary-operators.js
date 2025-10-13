9bad1c9f1d58de239a52c6e37c73c02996a1568e
Javier Pérez López <javier.perezlopez6@icloud.com>
2025-10-13 23:20:38 +0200

    Renaming functions and variables in ternary operator exercises to improve code clarity

--- 02-ternary-operators/02-ternary-operators.js
+++ 02-ternary-operators/02-ternary-operators.js
@@ -1,40 +1,40 @@
 // Exercise 1
-const puedeConducir = (edad) => edad >= 18 ? 'You can drive' : 'You cannot drive';
+const canDrive = (age) => age >= 18 ? 'You can drive' : 'You cannot drive';
 
 // HTML part to test
 const exercise1Test = document.getElementById('run-exercise-1-btn');
 exercise1Test.addEventListener('click', () => {
     
-    const result = puedeConducir(20);
+    const result = canDrive(20);
     console.log(result);
 });
 
 // Exercise 2
-const compararNumeros = (num1, num2) => num1 > num2 ? 'num1 is greater' : 'num2 is greater';
+const compareNumbers = (num1, num2) => num1 > num2 ? 'num1 is greater' : 'num2 is greater';
 
 // HTML part to test
 const exercise2Test = document.getElementById('run-exercise-2-btn');
 exercise2Test.addEventListener('click', () => {
     
-    const result = compararNumeros(10, 5);
+    const result = compareNumbers(10, 5);
     console.log(result);
 });
 
 // Exercise 3.1
-const clasificarNumero = (numero) => 
-    numero > 0 ? 'Positive' : 
-    numero < 0 ? 'Negative' : 'Zero';
+const classifyNumber = (number) => 
+    number > 0 ? 'Positive' : 
+    number < 0 ? 'Negative' : 'Zero';
 
 // HTML part to test
 const exercise3_1Test = document.getElementById('run-exercise-3-1-btn');
 exercise3_1Test.addEventListener('click', () => {
 
-    const result = clasificarNumero(5);
+    const result = classifyNumber(5);
     console.log(result);
 });
 
 // Exercise 3.2
-const encontrarMaximo = (a, b, c) => 
+const findMaximum = (a, b, c) => 
     a >= b && a >= c ? a : 
     b >= c ? b : c;
 
@@ -42,15 +42,15 @@ const encontrarMaximo = (a, b, c) =>
 const exercise3_2Test = document.getElementById('run-exercise-3-2-btn');
 exercise3_2Test.addEventListener('click', () => {
     
-    const result = encontrarMaximo(10, 5, 8);
+    const result = findMaximum(10, 5, 8);
     console.log(result);
 });
 
 // Exercise 4
-const parOImpar = (numeros) => {
-    for (let i = 0; i < numeros.length; i++) {
-        const resultado = numeros[i] % 2 === 0 ? 'Even' : 'Odd';
-        console.log(`${numeros[i]} is ${resultado}`);
+const evenOrOdd = (numbers) => {
+    for (let i = 0; i < numbers.length; i++) {
+        const result = numbers[i] % 2 === 0 ? 'Even' : 'Odd';
+        console.log(`${numbers[i]} is ${result}`);
     }
 };
 
@@ -58,6 +58,6 @@ const parOImpar = (numeros) => {
 const exercise4Test = document.getElementById('run-exercise-4-btn');
 exercise4Test.addEventListener('click', () => {
     
-    const numeros = [1, 2, 3, 4, 5, 6];
-    parOImpar(numeros);
+    const numbers = [1, 2, 3, 4, 5, 6];
+    evenOrOdd(numbers);
 });
